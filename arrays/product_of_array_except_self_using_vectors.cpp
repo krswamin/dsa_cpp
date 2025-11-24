45960abd26f915bf23c07db68d6458e30d5e8f1f
krswamin <krswamin@gmail.com>
2025-11-24 11:07:53 -0500

    feat(array): product except self

---
+++ arrays/product_of_array_except_self_using_vectors.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> product_except_self(const vector<int> &arr);
+void print_array(const vector<int> &arr);
+
+int main(){
+    vector<vector <int>> arrs = {
+      {-1,10,8,7,-6,6},
+      {2,3,-8,7,-1,2}};
+
+    vector<int> product;
+    for(auto &arr:arrs){  
+        cout<<"\nOriginal Array     :";
+        print_array(arr);   
+
+        product = product_except_self(arr);
+        cout<<"Product except self:";
+        print_array(product);
+    }
+}
+
+
+vector<int> product_except_self(const vector<int> &arr){
+    vector<int> left_product;
+    vector<int> right_product(arr.size());
+    vector<int> product;
+
+    left_product.reserve(arr.size());
+    product.reserve(arr.size());
+
+    left_product.push_back(1);
+    right_product[arr.size()-1] = 1;
+    for(int i=1; i<arr.size(); i++){
+        left_product.push_back(arr[i-1]*left_product[i-1]);
+        right_product[arr.size()-1-i] = arr[arr.size()-i]*right_product[arr.size()-i];
+    }
+
+    for(int i=0; i<arr.size(); i++){
+        product.push_back(left_product[i]*right_product[i]);
+    }
+    
+    return product;
+
+}
+
+void print_array(const vector<int> &arr){
+    for(int x:arr){
+        cout<<x<<", ";
+    }
+    cout<<"\n";
+}
