3ae6d343b923ccc4fe9c31157c655fa4be6d05f3
krswamin <krswamin@gmail.com>
2025-11-25 19:39:21 -0500

    chore(sets): clang formatting

--- sets/ordered_set_basics.cpp
+++ sets/ordered_set_basics.cpp
@@ -1,54 +1,50 @@
-#include<iostream>
-#include<set>
+#include <iostream>
+#include <set>
 
 using namespace std;
 
-int main(){
-     set <string> set1 = {"Krithika",
-                          "random",
-                          "Vasanthi",
-                          "Swaminathan"
-                          };
-    
-    cout<<"\n set: before modifications (notice that it is sorted in ascending order)  \n";
-    for (auto x: set1){
-        cout<<x<<"\n";
-    }
-    // i) Search : key not found
-    auto it = set1.find("Saumil");
-    if (it != set1.end()){
-         cout<<"\n set: Saumil found:"<<*it<<"\n";
-    } else {
-         cout<<"\n set: Saumil not found";
-    }
-   
+int main() {
+  set<string> set1 = {"Krithika", "random", "Vasanthi", "Swaminathan"};
 
-    // ii) Deletion
-    set1.erase("random");
+  cout << "\n set: before modifications (notice that it is sorted in ascending "
+          "order)  \n";
+  for (auto x : set1) {
+    cout << x << "\n";
+  }
+  // i) Search : key not found
+  auto it = set1.find("Saumil");
+  if (it != set1.end()) {
+    cout << "\n set: Saumil found:" << *it << "\n";
+  } else {
+    cout << "\n set: Saumil not found";
+  }
 
-    // iii) Add using emplace
-    // Emplace is better than insert. More efficient    
-    set1.emplace("Soumya");
-    set1.emplace("Pranav");
+  // ii) Deletion
+  set1.erase("random");
 
-    // iv) Add using insert
-    // Insert expects a pair. If you have the data ready insert might be preferable
-    // But emplace is generally preferable
-    set1.insert("Saumil");
+  // iii) Add using emplace
+  // Emplace is better than insert. More efficient
+  set1.emplace("Soumya");
+  set1.emplace("Pranav");
 
-    cout<<"\n\n set: after modifications (notice that it is sorted in ascending order) \n";
-    for (auto x: set1){
-        cout<<x<<"\n";
-    }
+  // iv) Add using insert
+  // Insert expects a pair. If you have the data ready insert might be
+  // preferable But emplace is generally preferable
+  set1.insert("Saumil");
 
-    // v) Search : key found
-    it = set1.find("Saumil");
-    if (it != set1.end()){
-         cout<<"\n set: Saumil found:"<<*it<<"\n";
-    } else {
-         cout<<"\n set: Saumil not found:\n";
-    }
+  cout << "\n\n set: after modifications (notice that it is sorted in "
+          "ascending order) \n";
+  for (auto x : set1) {
+    cout << x << "\n";
+  }
 
-    return 0;
+  // v) Search : key found
+  it = set1.find("Saumil");
+  if (it != set1.end()) {
+    cout << "\n set: Saumil found:" << *it << "\n";
+  } else {
+    cout << "\n set: Saumil not found:\n";
+  }
 
+  return 0;
 }
--- sets/unordered_set_basics.cpp
+++ sets/unordered_set_basics.cpp
@@ -1,54 +1,51 @@
-#include<iostream>
-#include<unordered_set>
+#include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
-int main(){
-     unordered_set <string> set1 = {"Krithika",
-                          "random",
-                          "Vasanthi",
-                          "Swaminathan"
-                          };
-    
-    cout<<"\n Unordered Set: before modifications (notice that it is NOT sorted)  \n";
-    for (auto x: set1){
-        cout<<x<<"\n";
-    }
-    // i) Search : key not found
-    auto it = set1.find("Saumil");
-    if (it != set1.end()){
-         cout<<"\n Unordered Set: Saumil found:"<<*it<<"\n";
-    } else {
-         cout<<"\n Unordered Set: Saumil not found";
-    }
-   
+int main() {
+  unordered_set<string> set1 = {"Krithika", "random", "Vasanthi",
+                                "Swaminathan"};
 
-    // ii) Deletion
-    set1.erase("random");
+  cout << "\n Unordered Set: before modifications (notice that it is NOT "
+          "sorted)  \n";
+  for (auto x : set1) {
+    cout << x << "\n";
+  }
+  // i) Search : key not found
+  auto it = set1.find("Saumil");
+  if (it != set1.end()) {
+    cout << "\n Unordered Set: Saumil found:" << *it << "\n";
+  } else {
+    cout << "\n Unordered Set: Saumil not found";
+  }
 
-    // iii) Add using emplace
-    // Emplace is better than insert. More efficient    
-    set1.emplace("Soumya");
-    set1.emplace("Pranav");
+  // ii) Deletion
+  set1.erase("random");
 
-    // iv) Add using insert
-    // Insert expects a pair. If you have the data ready insert might be preferable
-    // But emplace is generally preferable
-    set1.insert("Saumil");
+  // iii) Add using emplace
+  // Emplace is better than insert. More efficient
+  set1.emplace("Soumya");
+  set1.emplace("Pranav");
 
-    cout<<"\n\n Unordered Set: after modifications (notice that it is NOT sorted) \n";
-    for (auto x: set1){
-        cout<<x<<"\n";
-    }
+  // iv) Add using insert
+  // Insert expects a pair. If you have the data ready insert might be
+  // preferable But emplace is generally preferable
+  set1.insert("Saumil");
 
-    // v) Search : key found
-    it = set1.find("Saumil");
-    if (it != set1.end()){
-         cout<<"\n Unordered Set: Saumil found:"<<*it<<"\n";
-    } else {
-         cout<<"\n Unordered Set: Saumil not found:\n";
-    }
+  cout << "\n\n Unordered Set: after modifications (notice that it is NOT "
+          "sorted) \n";
+  for (auto x : set1) {
+    cout << x << "\n";
+  }
 
-    return 0;
+  // v) Search : key found
+  it = set1.find("Saumil");
+  if (it != set1.end()) {
+    cout << "\n Unordered Set: Saumil found:" << *it << "\n";
+  } else {
+    cout << "\n Unordered Set: Saumil not found:\n";
+  }
 
+  return 0;
 }
