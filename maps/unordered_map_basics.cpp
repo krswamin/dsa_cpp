9be22fb15549b6bda6e58b8e5620d5f226f562a1
krswamin <krswamin@gmail.com>
2025-11-25 19:39:07 -0500

    chore(maps): clang formatting

--- maps/unordered_map_basics.cpp
+++ maps/unordered_map_basics.cpp
@@ -1,54 +1,53 @@
-#include<iostream>
-#include<unordered_map>
+#include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-int main(){
-     unordered_map <int,string> map1 = {{11,"Krithika"},
-                          {53,"random"},
-                          {17,"Vasanthi"},
-                          {23,"Swaminathan"}
-                          };
-    
-    cout<<"\n Unordered Map: before modifications (notice that it is NOT sorted) \n";
-    for (auto x: map1){
-        cout<<x.first<<":"<<x.second<<"\n";
-    }
-    // i) Search : key not found
-    auto it = map1.find(9);
-    if (it != map1.end()){
-         cout<<"\n Unordered Map: Saumil found:"<<it->first<<","<<it->second<<"\n";
-    } else {
-         cout<<"\n Unordered Map: Saumil not found";
-    }
-   
+int main() {
+  unordered_map<int, string> map1 = {
+      {11, "Krithika"}, {53, "random"}, {17, "Vasanthi"}, {23, "Swaminathan"}};
 
-    // ii) Deletion
-    map1.erase(53);
+  cout << "\n Unordered Map: before modifications (notice that it is NOT "
+          "sorted) \n";
+  for (auto x : map1) {
+    cout << x.first << ":" << x.second << "\n";
+  }
+  // i) Search : key not found
+  auto it = map1.find(9);
+  if (it != map1.end()) {
+    cout << "\n Unordered Map: Saumil found:" << it->first << "," << it->second
+         << "\n";
+  } else {
+    cout << "\n Unordered Map: Saumil not found";
+  }
 
-    // iii) Add using emplace
-    // Emplace is better than insert. More efficient
-    map1.emplace(6, "Pranav");
-    map1.emplace(1, "Soumya");
+  // ii) Deletion
+  map1.erase(53);
 
-    // iv) Add using insert
-    // Insert expects a pair. If you have the data ready insert might be preferable
-    // But emplace is generally preferable
-    map1.insert({9, "Saumil"});
+  // iii) Add using emplace
+  // Emplace is better than insert. More efficient
+  map1.emplace(6, "Pranav");
+  map1.emplace(1, "Soumya");
 
-    cout<<"\n\n Unordered Map: after modifications (notice that it is NOT sorted)  \n";
-    for (auto x: map1){
-        cout<<x.first<<":"<<x.second<<"\n";
-    }
+  // iv) Add using insert
+  // Insert expects a pair. If you have the data ready insert might be
+  // preferable But emplace is generally preferable
+  map1.insert({9, "Saumil"});
 
-    // v) Search : key found
-    it = map1.find(9);
-    if (it != map1.end()){
-         cout<<"\n Unordered Map: Saumil found:"<<it->first<<","<<it->second<<"\n";
-    } else {
-         cout<<"\n Unordered Map: Saumil not found \n";
-    }
+  cout << "\n\n Unordered Map: after modifications (notice that it is NOT "
+          "sorted)  \n";
+  for (auto x : map1) {
+    cout << x.first << ":" << x.second << "\n";
+  }
 
-    return 0;
+  // v) Search : key found
+  it = map1.find(9);
+  if (it != map1.end()) {
+    cout << "\n Unordered Map: Saumil found:" << it->first << "," << it->second
+         << "\n";
+  } else {
+    cout << "\n Unordered Map: Saumil not found \n";
+  }
 
+  return 0;
 }
